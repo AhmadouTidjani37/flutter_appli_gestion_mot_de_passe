b9d039bcf559259a4278ab9a9fb0448f52999d2d
Ahmadou Tidjani <125843558+AhmadouTidjani37@users.noreply.github.com>
2026-03-16 13:30:07 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-// main.dart
 import 'package:flutter/material.dart';
-import 'package:gest_mdp/firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'services/auth_service.dart';
 import 'services/database_service.dart';
@@ -10,15 +8,9 @@ import 'screens/login_screen.dart';
 import 'screens/admin/admin_dashboard.dart';
 import 'screens/user/user_dashboard.dart';
 import 'screens/splash_screen.dart'; 
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  
 
   final databaseService = DatabaseService();
   await databaseService.database;
